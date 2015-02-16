369d00fbaa1a1586a30797d7343b27b25a37cc82
Joey  Caughey <joey@onemethod.com>
2015-02-16 16:21:12 -0500

    first commit

---
+++ jquery.content.js
@@ -0,0 +1,125 @@
+var Content = {
+    html_templates: [],
+    replacement_values: [],
+    init: function(options) {
+        $("img").each(function() {
+            if (typeof $(this).data("src") != "undefined") {
+                var src = $(this).data("src");
+                $(this).attr("src", src.replace(":uploads_folder:", API.get_uploads_folder()));
+            }
+        })
+    },
+
+    loop: function(object, records) {
+        var self = this;
+        
+        if (!self.html_template[object]) {
+            self.html_template[object] = $('[data-content-loop=' + object + ']').html();
+        }
+
+        if (records.length === 0) {
+            $('[data-content-loop=' + object + ']').hide();
+        } else {
+
+            $('[data-content-loop=' + object + ']').each(function() {
+                var loop_object = $(this);
+                $(this).empty();
+
+                $.each(records, function(key, values) {
+                    var html = self.html_template[object];
+
+                    values.count = key+1;
+                    html = self.parse_tags(object, html, values);
+                    //console.log(html_template, object, html, values);
+
+                    loop_object.append(html).show(0);
+                })
+            }).css("visibility", "visible");
+        }
+    },
+    reload: function(object, records) {
+        var self = this;
+        $('[data-content-loop=' + object + ']').html(self.html_template[object]);
+        self.loop(object, records);
+    },
+    parse: function(object, values) {
+        var self = this;
+        var html = $("#" + object).html();
+        $(object).css("visibility", "hidden");
+        html = self.parse_tags(object, html, values);
+        $(object).css("visibility", "show");
+        $("#" + object).html(html);
+        $("article > section").show();
+    },
+
+    find_all_tags: function(str) {
+        // "\{.*?\}"
+        var results = [],
+            re = /{([^}]+)}/g,
+            text;
+
+        while (text = re.exec(str)) {
+            results.push(text[1]);
+        }
+        return results;
+    },
+    parse_values: function(object, values, nodes) {
+        var self = this;
+
+        $.each(values, function(key, value) {
+            newNodes = nodes ? nodes.slice() : [];
+            newNodes.push(key);
+
+            if (typeof(value) === "object") {
+                self.parse_values(object, value, newNodes);
+            } else {
+                //console.log(parse_nodes(newNodes), value);
+                var replacer = object + '.' + self.parse_nodes(newNodes);
+                self.replacement_values[replacer] = value;
+            }
+        });
+    },
+    parse_nodes: function(nodes) {
+        var output = "";
+        $.each(nodes, function(i, node) {
+            if (i === 0) {
+                output += node;
+            } else {
+                output += "." + node;
+            }
+        })
+        return output;
+    },
+    parse_tags: function(object, input, values) {
+        var self = this;
+        
+       
+        self.parse_values(object, values);
+        
+
+        $.each(self.find_all_tags(input), function(i, tag) {
+            var split = tag.split("|");
+            var key = split[0];
+
+            if (split[1]) {
+                var action = split[1].toString().trim();
+                //console.log(action);
+                var value = window[action](self.replacement_values[key]);
+            } else {
+                var value = self.replacement_values[key];
+            }
+            //console.log("Tags", tag, value);
+
+            var replacer = '{' + tag + '}';
+            input = input.replaceAll(replacer, value);
+        });
+
+        return input;
+    }
+}
+
+$(window).bind('hashchange', function() {   
+    Content.html_template = [];
+}).load(function() {
+    Content.html_template = [];
+});
---
+++ jquery.format.js
@@ -0,0 +1,104 @@
+var Format = {
+	slug: function(text) {
+	    return text
+	        .toLowerCase()
+	        .replace(/ /g,'-')
+	        .replace(/---/g,'-')
+	        .replace(/--/g,'-')
+	        .replace(/[^\w-]+/g,'')
+	        ;
+	}
+}
+
+function FORMAT_truncate30(text) {
+	return (text.length>30) ? text.substring(0, 30)+"..." : text;
+}
+
+function FORMAT_lowercase(text) {
+	return text.toLowerCase();
+}
+
+
+
+function FORMAT_slug(text) {
+	return Format.slug(text);
+}
+
+
+function FORMAT_archive_date_slug(date) {
+	date = moment(date).format("YYYY-MM");
+	return date;
+}
+
+function FORMAT_truncate_event_title(title) {
+	return title.substring(0, 70);
+}
+
+function FORMAT_truncate_event_details(content) {
+	return (content.length>80) ? content.substring(0, 80)+"..." : content;
+}
+
+function FORMAT_truncate_news_details(content) {
+	return (content.length>220) ? content.substring(0, 220)+"..." : content;
+}
+
+function FORMAT_parse_links(text) {
+    var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/i;
+    return text.replace(exp, "<a href='$1'>$1</a>"); 
+}
+
+
+// ** DATE AND TIME FORMATS ** //
+
+
+function FORMAT_from_ago(date){
+	timestamp = new Date(Date.parse(date.replace(/( +)/, ' UTC$1')));
+	return moment(timestamp).fromNow();
+}
+
+function FORMAT_timestamp(date) {
+	date = moment.unix(date).format("DD MMMM, YYYY");
+	return date;
+}
+
+function FORMAT_date_time(date) {
+	date = moment.unix(date).format("MMMM DD, YYYY H:mma");
+	return date;
+}
+
+function FORMAT_archive_date(date) {
+	date = moment(date).format("MMMM YYYY");
+	return date;
+}
+
+function FORMAT_month(date) {
+	date = moment(date).format("MMMM");
+	return date;
+}
+
+function FORMAT_year(date) {
+	date = moment(date).format("YYYY");
+	return date;
+}
+
+
+function FORMAT_event_date_short(date) {
+	date = moment(date).format("MMM Do YYYY");
+	return date;
+}
+
+function FORMAT_event_date_long(date) {
+	date = moment(date).format("MMMM Do YYYY");
+	return date;
+}
+
+function FORMAT_event_timestamp(date) {
+	date = moment.unix(date).toISOString();
+	return date;
+}
+
+function FORMAT_time(time) {
+	date = moment(moment().format('YYYY-MM-DD')+" "+ time).format("H:mma");
+	return date;
+}
+
